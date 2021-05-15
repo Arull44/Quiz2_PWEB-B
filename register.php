9088e86e3dfcb75399944b4f5718a9dc4cd92695
Arull44 <63081155+Arull44@users.noreply.github.com>
2021-05-15 15:43:00 +0700

    Update-1

--- register.php
+++ register.php
@@ -92,7 +92,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <style>
         body {
-            background-color:grey;
+            background-image: url("coding.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: scroll;
         }
 
         .errorMsg{
